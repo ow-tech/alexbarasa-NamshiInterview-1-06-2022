878f6202c9d5baf1181257c511e0e3b06eb5d1b4
ow-tech <wagwanwekon@outlook.com>
2022-06-01 19:46:50 +0400

    ready to deploy

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react'
 import Board from './Board.js';
 
 export default class Matrix extends Component {
-  handleClick() {
+  handleClick(i) {
+    const history = this.state.history.slice(0, this.state.stepNumber+1)
+    const current = history[history.length -1];
+    const squares =current.squares.slice()
+    squares[i] = this.state.xIsNext? 'X':'0';
+    this.setState({
+      history:history.concat({
+        squares : squares
+
+      })
+  })
+    // alert(i)
+    console.log(i)
     // const history = this.state.history.slice(0, this)
   }
     constructor (props){
         super(props);
         this.state ={
+          xIsNext:  true,
           stepNumber:0,
             history: [
               {squares: Array(16).fill(null)}
